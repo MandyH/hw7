a184100a2d5ee215c72bf14f25a8ccfbd28b4f10
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2013-07-05 00:01:19 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -3,65 +3,90 @@
 
 using namespace std;
 
-Card::Card(int &suit, int &face):_suit(suit),_face(face) {}
+Card::Card(int suit, int face):_suit(suit),_face(face) {}
 
-int Card::suit() 
-{   return this->_suit;  }
+Card::Card(Card &c):_suit(c.suit()),_face(c.face()) {}
 
-int Card::face() 
-{   return this->_face;  }
+int Card::suit()
+{   return _suit;  }
 
-void Player::deal(Player &p1, Player &p2, Player &p3, Player &p4) 
-{    for (int i=0; i<4; i++) 
-     {  for (int j=0; j<13; j++) 
-        {  do 
-           {  int again = 0;
-              int giveToPlayer = rand() % 4;
-              switch (giveToPlayer) 
-              {  case 0:
-                      if (!p1.isFull()) 
-                      {  Card c(i,j);
-                         p1.add(c);  } 
-                      else 
-                      {  again = 1;  }
-                 break;
-                 case 1:
-                      if (!p2.isFull()) 
-                      {  Card c(i,j);
-                         p2.add(c);  } 
-                      else 
-                      {  again = 1;  }
-                 break;
-                 case 2:
-                      if (!p3.isFull()) 
-                      {  Card c(i,j);
-                         p3.add(c);  } 
-                      else 
-                      {  again = 1;  }
-                      break;
-                 case 3:
-                      if (!p4.isFull()) 
-                      {  Card c(i,j);
-                         p4.add(c);  } 
-                      else 
-                      {  again = 1;  }
-                      break;  }  } 
-              while (again);  }  }  }
-       
+int Card::face()
+{   return _face;  }
 
-bool Player::isFull()
-{  if (_onHold==13) 
-   {  return true;  } 
-   else 
-   {  return false;  }  }
-   
-bool Player::void add(Card &c)
-{  if (!isFull) 
-   {  _deck[_onHold] = c;
-      _onHold++;  }  }
 
-ostream& operator<<(ostream&out, const Player print)
-{  for (int i = 0; i < print.i; i++)  
-   {  for (int j = 0; j < print.i; j++)
-        cout << print.c(i,j) << endl;  }  
-   return cout;  }
+Player::Player() {
+    _onHold = 0;
+    for (int i=0; i<13; i++)
+        _deck[i] = Card();
+}
+
+void Player::deal(Player &p1, Player &p2, Player &p3, Player &p4) {
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<13; j++) {
+            int again = 0;
+            do {
+                int giveToPlayer = rand() % 4;
+                switch (giveToPlayer){
+                    case 0:
+                        if (!p1.isFull()) {
+                            Card c(i,j);
+                            p1.add(c);
+                        } else {
+                            again = 1;
+                        }
+                        break;
+                    case 1:
+                        if (!p2.isFull()) {
+                            Card c(i,j);
+                            p2.add(c);
+                        } else {
+                            again = 1;
+                        }
+                        break;
+                    case 2:
+                        if (!p3.isFull()) {
+                            Card c(i,j);
+                            p3.add(c);
+                        } else {
+                            again = 1;
+                        }
+                        break;
+                    case 3:
+                        if (!p4.isFull()) {
+                            Card c(i,j);
+                            p4.add(c);
+                        } else {
+                            again = 1;
+                        }
+                        break;
+                }
+            } while (again);
+        }
+    }
+}
+
+
+bool Player::isFull() {return _onHold==13;}
+
+void Player::add(Card &c) {
+    if (!isFull()) {
+        _deck[_onHold] = c;
+        _onHold++;
+    }
+}
+
+Card& Player::operator[] (int i) {
+    return _deck[i];
+}
+
+ostream& operator<<(ostream&out, Card &c) {
+    out << c.suit() << c.face();
+    return out;
+}
+
+ostream& operator<<(ostream&out, Player &p) {
+    for (int i=0; i<13; i++) {
+        out << p[i] << endl;
+    }
+    return out;
+}
