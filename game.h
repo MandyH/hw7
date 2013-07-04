dd62f48bd0ffcdfbb608525a45edce0658edfb5c
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2013-07-05 00:01:35 +0800

    Update game.h

--- game.h
+++ game.h
@@ -1,30 +1,39 @@
+#ifndef _Poker_Card
+#define _Poker_Card
+
 #include <iostream>
 
 using namespace std;
 
-class Card 
-{  private:
+class Card {
+private:
     int _suit;
     int _face;
-   public:
-    Card(int &suit, int &face); 
+public:
+    Card(int =0, int =0);
+    Card(Card &c);
     int suit();
-    int face(); };
+    int face();
+    //friend ostream& operator<<(ostream&out, const Card &c);
+};
 
-class Table 
-{  private:
-    Card _deck[4];  };
+class Table {
+private:
+    Card _deck[4];
+};
 
-class Player 
-{  private:
+class Player {
+private:
     Card _deck[13];
     int _onHold;
-   public:
-    Player():_onHold(0); 
+public:
+    Player();
     bool Check();
     void add(Card &c);
-    bool isFull(); 
+    bool isFull();
     void deal(Player &p1, Player &p2, Player &p3, Player &p4);
-    friend ostream& operator<<(ostream&out, const Player print);  };
+    Card& operator[] (int);
+    //friend ostream& operator<<(ostream&out, const Player &p);
+};
 
-
+#endif
